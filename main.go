301113a5606d6d76045f48bf36874e748a9c00b2
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2025-10-14 05:38:07 +0900

    dotenv configure added.

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"io"
 	"log"
+	"os"
 
 	"sshchat/utils"
 
 	"github.com/gliderlabs/ssh"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
 	keys, err := utils.CheckHostKey()
 	if err != nil {
 		log.Print("Failed to check SSH keys: generate one.\n", err)
@@ -29,12 +38,13 @@ func main() {
 	}
 
 	s := &ssh.Server{
-		Addr:    ":2222",
+		Addr:    ":" + port,
 		Handler: sessionHandler,
 	}
 	for _, key := range keys {
 		s.AddHostKey(key)
 	}
 
+	log.Print("Listening on :" + port)
 	log.Fatal(s.ListenAndServe())
 }
